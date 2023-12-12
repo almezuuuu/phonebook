8c7332875fefc82df99c3ba1ef476a2b6ed445c4
Karl Alob <karlmarie.alob@gmail.com>
2023-12-12 21:30:34 +0800

    added db file. check db folder.

--- db/phonebook.sql
+++ db/phonebook.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2022 at 04:07 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.30
+-- Generation Time: Dec 12, 2023 at 02:29 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,8 +40,55 @@ CREATE TABLE `tbl_contacts` (
 --
 
 INSERT INTO `tbl_contacts` (`ID`, `Fname`, `Lname`, `Cnum`, `Date_created`) VALUES
-(3, 'angelo', 'morancil', '09499679169', '2022-06-06 21:43:02'),
-(4, 'jerelyn', 'mallo', '09165456499', '2022-06-06 21:43:16');
+(3, 'Angelo', 'Morancil', '09499679169', '2022-06-06 21:43:02'),
+(7, 'Joseph', 'Huelgas', '09289342924', '2023-01-30 11:49:07'),
+(9, 'test', 'test', '9898576576', '2023-03-27 09:33:49'),
+(10, 'Sean', 'Cruz', '123456', '2023-07-31 11:21:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_course`
+--
+
+CREATE TABLE `tbl_course` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_enrolled`
+--
+
+CREATE TABLE `tbl_enrolled` (
+  `ID` int(11) NOT NULL,
+  `sID` int(11) NOT NULL,
+  `cID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `ID` int(10) NOT NULL,
+  `Fname` varchar(255) NOT NULL,
+  `Lname` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_student`
+--
+
+INSERT INTO `tbl_student` (`ID`, `Fname`, `Lname`, `Address`, `Email`) VALUES
+(1, 'sean', 'crux', 'adw', 'ela');
 
 --
 -- Indexes for dumped tables
@@ -54,6 +101,24 @@ ALTER TABLE `tbl_contacts`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbl_enrolled`
+--
+ALTER TABLE `tbl_enrolled`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,7 +126,25 @@ ALTER TABLE `tbl_contacts`
 -- AUTO_INCREMENT for table `tbl_contacts`
 --
 ALTER TABLE `tbl_contacts`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_enrolled`
+--
+ALTER TABLE `tbl_enrolled`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
